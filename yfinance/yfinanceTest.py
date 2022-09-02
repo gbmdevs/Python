e37410188550d0f5b42b4022d80f7bb6215906e4
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2022-09-02 14:53:27 +0000

    Att python

--- yfinance/yfinanceTest.py
+++ yfinance/yfinanceTest.py
@@ -2,11 +2,11 @@
 import yfinance as yf
 
 #Lista de Ativos da Bolsa para Teste
-ativos = ['ITSA4.SA','USIM5.SA','BBAS3.SA','HASH11.SA','MXRF11.SA','TRIS3.SA']
+ativos = ['ITSA4.SA','USIM5.SA','BBAS3.SA','TRPL4.SA','MXRF11.SA','TRIS3.SA']
 
 # Loop de geração de arquivos
 for ativo in ativos:
-    data = yf.download(ativo, group_by="Ticker", period='2022-08-31')
+    data = yf.download(ativo, group_by="Ticker", start="2022-09-02", end="2022-09-03")
     data['ticker'] = ativo
     data.to_csv(f'ativo_{ativo}.csv')
     print(data)
